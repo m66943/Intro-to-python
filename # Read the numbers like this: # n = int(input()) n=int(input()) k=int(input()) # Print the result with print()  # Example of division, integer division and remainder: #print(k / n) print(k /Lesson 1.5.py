e7c0405030064db5a31a21d35e3af549e2fca492
m66943 <144810465+m66943@users.noreply.github.com>
2023-09-22 09:39:44 +0100

    Create Lesson 1.5.py

---
+++ # Read the numbers like this: # n = int(input()) n=int(input()) k=int(input()) # Print the result with print()  # Example of division, integer division and remainder: #print(k / n) print(k /Lesson 1.5.py
@@ -0,0 +1,10 @@
+# Read the numbers like this:
+# n = int(input())
+n=int(input())
+k=int(input())
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+#print(k / n)
+print(k // n)
+print(k % n)
