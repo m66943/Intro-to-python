beb633ad89fd8cbb6676f9fb0038639447f20abf
m66943 <144810465+m66943@users.noreply.github.com>
2023-09-22 09:27:30 +0100

    Create Lesson 1.4.py

---
+++ Lesson 1.4.py
@@ -0,0 +1,8 @@
+# Read an integer:
+# a = int(input())
+# Print a value:
+# print(a)
+n = int(input())
+print(n)
+print("The next number for the number", n ,"is", n+1)
+print("The previous number for the number", n, "is", n-1)
